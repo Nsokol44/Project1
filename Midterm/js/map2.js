40782ffe382fa3cffb59392be964779ab391fb06
Nicholas J. Sokol <Nsokol44@gmail.com>
2016-03-16 15:50:40 -0500

    Update map2.js

--- Midterm/js/map2.js
+++ Midterm/js/map2.js
@@ -17,6 +17,26 @@ function initMap() {
     zoom: 4,
     center: {lat: 30, lng: -90}
   });
+  
+  // Add a GeoJSON file to the map.
+map.data.loadGeoJson('https://opendata.arcgis.com/datasets/11a4c0d646544b3ba7290c100dacfa8a_0.geojson');
+
+// Create an infowindow object to use later
+var infowindow = new google.maps.InfoWindow();
+
+/* Create a "listener" that will wait for the user to click an earthquake point,
+ * and then display the infowindow with details about that earthquake.
+ */
+map.data.addListener('click', function(event) {
+  // in the geojson feature that was clicked, get the "place" and "mag" attributes
+  var place = event.feature.getProperty("STATION");
+  var dew = event.feature.getProperty("YR5_DEW_JAN_ALL");
+  var html = dew + ' Dew Point, ' + place; // inserting additional text between them
+  infowindow.setContent(html); // show the html variable in the infowindow
+  infowindow.setPosition(event.feature.getGeometry().get()); // anchor the infowindow at the marker
+  infowindow.setOptions({pixelOffset: new google.maps.Size(0,-30)}); // move the infowindow up slightly to the top of the marker icon
+  infowindow.open(map);
+});
 
 //Create layers for each of the KML files and geoJSON files.
 layers[0] = new google.maps.KmlLayer({
