8d5c519b8a3447b782e80f4e9359fce294862616
Nicholas J. Sokol <Nsokol44@gmail.com>
2016-03-16 09:55:43 -0500

    Update map3.js

--- Midterm/js/map3.js
+++ Midterm/js/map3.js
@@ -1,3 +1,4 @@
+ // Establish a map.
      var map;  
       require([  
         "esri/map", "esri/dijit/Scalebar", "esri/layers/ArcGISDynamicMapServiceLayer",  
@@ -11,17 +12,17 @@
           center: [-90, 30],  
           zoom: 4  
         });  
-
+//Create a toggle to switch between the two basemap layers.
        var toggle = new BasemapToggle({
         map: map,
         basemap: "satellite"
         }, "BasemapToggle");
         toggle.startup();
-
+//Add a drought layer to the map.
         var droughtMapServiceLayer = new ArcGISDynamicMapServiceLayer("http://services.gis.ca.gov/arcgis/rest/services/AtmosphereClimate/DroughtMonitor/MapServer");
 
         map.addLayer(droughtMapServiceLayer);
-  
+  //Add a scalebar to the map.
         var scalebar = new Scalebar({  
           map: map,  
           scalebarUnit: "dual"  
