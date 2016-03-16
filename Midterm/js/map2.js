4330911609f328518c1f4223734a5f01068ed327
Nicholas J. Sokol <Nsokol44@gmail.com>
2016-03-16 09:42:35 -0500

    Update map2.js

--- Midterm/js/map2.js
+++ Midterm/js/map2.js
@@ -1,7 +1,8 @@
-
+//JS for weather google map.
 var map;
 var layers = [];
 
+//Create a function for toggles at the top of the map.
 function toggleLayer(i) {
   if(layers[i].getMap() === null) {
     layers[i].setMap(map);
@@ -10,13 +11,16 @@ function toggleLayer(i) {
     layers[i].setMap(null);
   }
 }
+//create a function to initialize the map.
 function initMap() {
    map = new google.maps.Map(document.getElementById('map'), {
     zoom: 4,
     center: {lat: 30, lng: -90}
   });
 
+map.data.loadGeoJson('http://data.wake.opendata.arcgis.com/datasets/e971bec1f1d94c9d927548b6d45455d4_0.geojson');
 
+//create layers that will go in the toggle.
 layers[0] = new google.maps.KmlLayer({
     url: 'http://www.srh.noaa.gov/gis/kml/raws/rawslink.kml',
     map: map,
