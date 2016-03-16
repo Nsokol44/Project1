fb667d0610bae49b53bda2a45b8e1588dcfc8743
Nicholas J. Sokol <Nsokol44@gmail.com>
2016-03-16 15:38:53 -0500

    Update map2.js

--- Midterm/js/map2.js
+++ Midterm/js/map2.js
@@ -45,5 +45,24 @@ layers[1] = new google.maps.KmlLayer({
     url: 'http://www.srh.noaa.gov/gis/kml/cod/susLink.kml',
     map: map,
 preserveViewport: true});
+layers[2] = map.data.loadGeoJson('https://opendata.arcgis.com/datasets/11a4c0d646544b3ba7290c100dacfa8a_0.geojson');
+
+// Create an infowindow object to use later
+var infowindow = new google.maps.InfoWindow();
+
+/* Create a "listener" that will wait for the user to click an earthquake point,
+ * and then display the infowindow with details about that earthquake.
+ */
+map.data.addListener('click', function(event) {
+  // in the geojson feature that was clicked, get the "place" and "mag" attributes
+  var place = event.feature.getProperty("STATION");
+  var dew = event.feature.getProperty("YR5_DEW_JAN_ALL");
+  var html = dew + ' Dew Point, ' + place; // inserting additional text between them
+  infowindow.setContent(html); // show the html variable in the infowindow
+  infowindow.setPosition(event.feature.getGeometry().get()); // anchor the infowindow at the marker
+  infowindow.setOptions({pixelOffset: new google.maps.Size(0,-30)}); // move the infowindow up slightly to the top of the marker icon
+  infowindow.open(map);
+});
+})
 }
 
